e20c3ea97445b116399d2ed90603aba348d4457b
Igor Risteski <pirej30@yahoo.com>
2021-11-02 00:50:10 +0100

    Change color onClick via useContext

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -3,13 +3,11 @@ import { useContext } from "react";
 import { ThemeContext } from "../context/ThemeContext";
 
 function Navbar() {
-  const context = useContext(ThemeContext);
-  // const { color } = useContext(ThemeContext);
+  const { color, changeColor } = useContext(ThemeContext);
 
   return (
-    <div className="Nav">
-      <h2 style={{ color: context.color }}>Hi from Navbar</h2>
-      {/* <h2 style={{ color: color }}>Hi from Navbar</h2> */}
+    <div className="Nav" style={{ color: color }}>
+      <h2 onClick={changeColor}>Hi from Navbar</h2>
     </div>
   );
 }
--- src/context/ThemeContext.js
+++ src/context/ThemeContext.js
@@ -1,10 +1,16 @@
-import { createContext } from "react";
+import { createContext, useState } from "react";
 
 export const ThemeContext = createContext();
 
 export function ThemeProvider({ children }) {
+  const [color, setColor] = useState("blue");
+
+  const changeColor = newColor => {
+    setColor("green");
+  };
+
   return (
-    <ThemeContext.Provider value={{ color: "blue" }}>
+    <ThemeContext.Provider value={{ color, changeColor }}>
       {children}
     </ThemeContext.Provider>
   );
